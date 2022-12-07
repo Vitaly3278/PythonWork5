2cc0702c11d24863d868e9b5276d0a1b316dba34
Vitaly Osipov <vital-nvl@mail.ru>
2022-12-07 19:42:16 +0300

    X_0

--- X_0.py
+++ X_0.py
@@ -1,47 +1,82 @@
-# Создайте программу для игры в ""Крестики-нолики"".
-
-print('-------- Игра КРЕСТИКИ НОЛИКИ----------\n')
+# Создайте программу для игры в ""Крестики-нолики""
 
 
-def board():
-    for i in range(3):
-        print('|', i * 3 + 1, '|', i * 3 + 2, '|', i * 3 + 3, '|')
+# Инициализация карты
+maps = [1, 2, 3,
+        4, 5, 6,
+        7, 8, 9]
 
-board()
-player = 1
-move2 =None
-player1 = []
-player2 = []
-
-
-def move(player):
-    x = int(input(f' Игрок {player} выберите свободную ячейку'))
-    return x
-
-
-def win(player):
-    win == ((1, 2, 3), (4, 5, 6), (7, 8, 9), (1, 4, 7), (2, 5, 8), (3, 6, 9), (1, 5, 9), (3, 5, 7))
-    for i in win:
-        if player[i[0]] == player[i[1]] == player[i[2]]:
-            return True
+# Инициализация победных линий
+victories = [[0, 1, 2],
+             [3, 4, 5],
+             [6, 7, 8],
+             [0, 3, 6],
+             [1, 4, 7],
+             [2, 5, 8],
+             [0, 4, 8],
+             [2, 4, 6]]
 
 
-while True:
-    for player in range(1, 3):
-        move = move(player)
-        player1.append(move)
-        print(f'Игрок {player} вы поставили Х на {move} ячейке')
-        win = win(player1)
-        if win == True:
-            print(f'Поздравляем игрок {player} вы выиграли!!!')
-            exit()
+# Вывод карты на экран
+def print_maps():
+    print(maps[0], end=" ")
+    print(maps[1], end=" ")
+    print(maps[2])
+
+    print(maps[3], end=" ")
+    print(maps[4], end=" ")
+    print(maps[5])
+
+    print(maps[6], end=" ")
+    print(maps[7], end=" ")
+    print(maps[8])
+
+
+# Сделать ход в ячейку
+def step_maps(step, symbol):
+    ind = maps.index(step)
+    maps[ind] = symbol
+
+
+# Получить текущий результат игры
+def get_result():
+    win = ""
+
+    for i in victories:
+        if maps[i[0]] == "X" and maps[i[1]] == "X" and maps[i[2]] == "X":
+            win = "X"
+        if maps[i[0]] == "O" and maps[i[1]] == "O" and maps[i[2]] == "O":
+            win = "O"
+
+    return win
+
+
+# Основная программа
+game_over = False
+player1 = True
+
+while game_over == False:
+
+    # 1. Показываем карту
+    print_maps()
+
+    # 2. Спросим у играющего куда делать ход
+    if player1 == True:
+        symbol = "X"
+        step = int(input("Игрок 1, ваш ход: "))
     else:
-        move = move(player)
-        player2.append(move)
-        print(f'Игрок {player+1} вы поставили 0 на {move} ячейке')
-        win = win(player2)
-        if win == True:
-            print(f'Поздравляем игрок {player}б вы выиграли!!!')
-            exit()
+        symbol = "O"
+        step = int(input("Игрок 2, ваш ход: "))
 
+    step_maps(step, symbol)  # делаем ход в указанную ячейку
+    win = get_result()  # определим победителя
+    if win != "":
+        game_over = True
+    else:
+        game_over = False
 
+    player1 = not (player1)
+
+# Игра окончена. Покажем карту. Объявим победителя.
+print_maps()
+print("Победил", win)
